2b8613792bad48e0463180b7decb34e0ca9c49af
mkesicki <2114710+mkesicki@users.noreply.github.com>
2023-09-04 00:12:09 +0200

    Cleanup and change the prompt

--- ai.py
+++ ai.py
@@ -7,14 +7,10 @@ from openai_functions import Conversation
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-def planner(city, country, pages="https://www.tripadvisor.com, https://www.thecrazytourist.com", max : int = 20, home : str = "Barcelona") :
+conversation = Conversation()
 
-    print("Let's plan trip to " + city.title() + " in " + country.title())
-
-    conversation = Conversation()
-
-    @conversation.add_function()
-    def getPlacesOfInterest(city: str, country: str, pages: str, max : int, home : str) -> dict:
+@conversation.add_function
+def getPlacesOfInterest(city: str, country: str, pages: str, max : int, home : str) -> dict:
 
         """Get touristic places in a city and country.
 
@@ -34,12 +30,15 @@ def planner(city, country, pages="https://www.tripadvisor.com, https://www.thecr
             "home": home
         }
 
+def planner(city, country, max : int , home : str, pages : str = "https://www.tripadvisor.com, https://www.thecrazytourist.com") :
+
+    print("Let's plan trip to " + city.title() + " in " + country.title())
+
     prompt = """I want to visit {city} in {country}.
                 Please list the most important and popular tourist attractions.
-                During your search use recommendations from [pages].
+                During your search use recommendations from {pages}.
                 Provide name, short description, link to google maps, link to google and link to wikipedia. {{places}}
-                Return maximum  {max} attractions.
-                Provide links to {pages} you used to find information.
+                Return maximum {max} attractions.
                 Return as HTML with following structure (do not add code snippet):
                 <!DOCTYPE html>
                 <html>
@@ -55,36 +54,48 @@ def planner(city, country, pages="https://www.tripadvisor.com, https://www.thecr
                         <p><b>Points of interest:</b></p>
                         <ol class="places">
                             <li class="place">
-                             <dl class="name"><b>{{place name}}</b></dl>
-                             <dt class="description"><i>{{place description}}</i></dt>
-                            <dt class="links">
-                                <span class="map">{{place linkGMap}}</span> * <span class="google">{{ place linkGoogle}}</span> * <span class="wiki">{{ place linkWiki}}</span>
-                            </dt>
-                        </dl>
+                                <dl class="name"><b>{{place name}}</b></dl>
+                                    <dt class="description"><i>{{place description}}</i></dt>
+                                    <dt class="links">
+                                        <span class="map">{{place linkGMap}}</span> * <span class="google">{{ place linkGoogle}}</span> * <span class="wiki">{{ place linkWiki}}</span>
+                                    </dt>
+                                </dl>
                             </li>
                         <ol>
-                        <div>&nbsp;</div>
-                        <p><b>Reference pages:</b></p>
-                        <ul class="pages">
-                            <li class="page">{{page}}</li>
-                        </ul>
-                        <div class="wikiloc">&nbsp;</div>
-                        <object
-                            data-attachment="map.kml"
-                            data="name:map"
-                            type="text/text" />
+                        <div>[REPLACE HERE]</div>
                     </body>
                 </html>
-               """.format(city = city, country = country, pages = pages, home = home, max = max)
+               """.format(city = city, country = country, home = home, max = max, pages = pages)
 
     data =  conversation.ask(prompt)
 
-    print(data)
-
+    # sometimes reply is duplicated
     if data.count("<html>") > 1:
 
         m = re.search('(<!DOCTYPE html>.+</html>).', data, flags = re.S)
+        print("*** more than 1 html tag ***")
         if m is not None:
             data = m.group(0)
+
+    # removed this from prompt to see if it helps to get better results
+    pagesHTML = ""
+    for page in pages.split(","):
+         pagesHTML = pagesHTML + "<li class=\"page\"><a href=\"" + page + "\">" + page + "</a></li>"
+
+    insert = """
+        <div>&nbsp;</div>
+        <p><b>Reference pages:</b></p>
+        <ul class="pages">
+            {pagesHTML}
+        </ul>
+        <div class="wikiloc">&nbsp;</div>
+        <object
+            data-attachment="map.kml"
+            data="name:map"
+            type="text/text" />
+    """.format(pagesHTML = pagesHTML)
+    data = data.replace("<div>[REPLACE HERE]</div>", insert)
+
+    print(data)
 
     return data
