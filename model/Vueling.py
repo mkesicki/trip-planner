af3368137ce4d7d2a0c7ba69ecff067efe64ca5f
mkesicki <2114710+mkesicki@users.noreply.github.com>
2023-09-21 21:45:35 +0200

    Fix Vueling

--- model/Vueling.py
+++ model/Vueling.py
@@ -5,20 +5,23 @@ from dateutil.relativedelta import relativedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import *
 
 class Vueling:
 
     def parse(self, fromCity : str, fromCountry : str, toCity : str, toCountry : str, roundTrip : bool, startDate : datetime.date, endDate : datetime.date, adults : int, params : dict):
 
         # they count from 0 -> January is 0
-        start = startDate - relativedelta(months=1)
-        end = endDate - relativedelta(months=1)
+        startMonth = startDate.month - 1
+        endMonth = endDate.month - 1
 
-        startTrip = start.strftime(params.get("dateFormat"))
-        endTrip = end.strftime(params.get("dateFormat"))
+        startTrip = startDate.strftime(params.get("dateFormat"))
+        endTrip = endDate.strftime(params.get("dateFormat"))
+        startTrip = startTrip.replace("{X}", str(startMonth))
+        endTrip = endTrip.replace("{X}", str(endMonth))
 
         url = params.get("url")
-        config = params.get("params")
+        self.config= params.get("params")
 
         print("Open Browser " + url + " in browser")
         browser = webdriver.Firefox()
@@ -26,18 +29,22 @@ class Vueling:
         browser.implicitly_wait(10) # seconds
 
         # accept cookies
-        if "cookiesAccept" in config:
-            browser.find_element(By.ID, config.get("cookiesAccept")).click()
+        if "cookiesAccept" in self.config:
+            browser.find_element(By.ID, self.config.get("cookiesAccept")).click()
 
-        departure = browser.find_element(By.ID, config.get("departure"))
+        departure = browser.find_element(By.ID, self.config.get("departure"))
         departure.send_keys(fromCity)
         departure.send_keys(Keys.ENTER)
 
-        arrival = browser.find_element(By.ID, config.get("arrival"))
+        browser.implicitly_wait(2) # seconds
+
+        arrival = browser.find_element(By.ID, self.config.get("arrival"))
         arrival.send_keys(toCity)
         arrival.send_keys(Keys.ENTER)
 
-        time.sleep(5)
+        browser.implicitly_wait(2) # seconds
+
+        self.findDate(browser, startTrip)
 
         if roundTrip != "on":
             print("Handle one way trip")
@@ -46,17 +53,30 @@ class Vueling:
             browser.find_element(By.ID, "calendarDaysTable" + startTrip).click()
         else:
             browser.find_element(By.ID, "calendarDaysTable" + startTrip).click()
-            browser.implicitly_wait(20) # seconds
+            browser.implicitly_wait(5) # seconds
+            self.findDate(browser, endTrip)
             browser.find_element(By.ID, "calendarDaysTable" + endTrip).click()
 
         # handle passengers number
         currentAdults = 1
 
         while currentAdults < int(adults):
-            browser.find_element(By.ID, config.get("adults")).click()
+            browser.find_element(By.ID, self.config.get("adults")).click()
             currentAdults = currentAdults + 1
 
         #submit form
-        browser.find_element(By.ID, config.get("submit")).click()
+        browser.find_element(By.ID, self.config.get("submit")).click()
 
         return ""
+
+    def findDate(self, browser, date):
+
+        try:
+            date = browser.find_element(By.ID, "calendarDaysTable" + date)
+            print("Date found !")
+            return
+
+        except NoSuchElementException as e:
+            print("search month in calendar")
+            browser.find_element(By.ID, self.config.get("dateForward")).click()
+            self.findDate(browser, date)
