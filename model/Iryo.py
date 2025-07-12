f0ea73dab18cd26c6a228a4952174056099d8ecc
Michal <7rdsggtwp@mozmail.com>
2025-07-12 19:56:22 +0200

    Change Iryo to new page layoiut

--- model/Iryo.py
+++ model/Iryo.py
@@ -1,76 +1,64 @@
 import logging
-import time
+import requests
+import webbrowser
+import json
+import urllib.parse
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
 from .data_classes import SearchQuery
 
 class Iryo:
 
     def parse(self, query: SearchQuery):
+
         startTrip = query.start_date.strftime(query.params.get("dateFormat"))
         endTrip = query.end_date.strftime(query.params.get("dateFormat"))
-        config = query.params.get("params")
 
-        browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-        browser.get(query.params.get("url"))
-        time.sleep(2)
+        tripDirection =  "outboundAndInbound" if query.round_trip is True else "outbound"
 
-        # accept cookies
-        if "cookiesAccept" in config:
-            cookiesAccept = browser.find_element(By.XPATH, config.get("cookiesAccept"))
-            cookiesAccept.click()
+        headers = {
+            "Origin": "https://iryo.eu",
+            "Ocp-Apim-Subscription-Key": "7c9b9b1ea0fe4f0c9d1739fcbf8b5438",
+            "User-Agent": "PostmanRuntime/7.44.1"
+        }
+        stations = requests.get("https://api.iryo.eu/b2c/support/stations?mappings=true&mappings-usage=GTM&lang=es&kcClient=b2c&requestChannel=WEB", headers=headers)
 
-        time.sleep(2)
+        data = stations.json()
+        passengers = []
 
-        browser.find_element(By.XPATH, config.get("departure")).click()
-        stations = browser.find_elements(By.CSS_SELECTOR, ".menu__item span")
-        self.findStation(query.from_city, stations, browser)
+        adults = query.adults
+        for i in (1, adults + 1) :
+            passengers.append({"type":"AD","discountCards":[],"id":f"passenger_{i}"})
 
-        time.sleep(2)
+        origin = self.findStation(city=query.from_city, stations=data.get("data"))
+        destination = self.findStation(city=query.to_city, stations=data.get("data"))
 
-        browser.find_element(By.XPATH, config.get("arrival")).click()
-        stations = browser.find_elements(By.CSS_SELECTOR, ".menu__item span")
-        self.findStation(query.to_city, stations, browser)
+        travels = []
 
-        startDate = browser.find_element(By.XPATH, config.get("dateFrom"))
-        startDate.clear()
-        startDate.send_keys(startTrip)
-        startDate.send_keys(Keys.ENTER)
+        travels.append({"origin":origin.get("uicStationCode"),"destination":destination.get("uicStationCode"),"direction":"outbound","departure":startTrip})
 
-        if query.round_trip:
-            endDate = browser.find_element(By.XPATH, config.get("dateBack"))
-            endDate.clear()
-            endDate.send_keys(endTrip)
-            endDate.send_keys(Keys.ENTER)
-        else:
-            browser.find_element(By.XPATH, config.get("oneWay")).click()
+        if query.round_trip is True:
+            travels.append({"origin":destination.get("uicStationCode"),"destination":origin.get("uicStationCode"),"direction":"inbound","departure":endTrip})
 
-        if query.adults > 1:
-            browser.find_element(By.XPATH, "/html/body/app-root/main/b2c-view-home/div[2]/section/div/div[1]/b2c-main-search/ilsa-main-search/div/div[2]/ilsa-passengers-picker/div/div[1]").click()
+        params_dict = {
+            'passengers': json.dumps(passengers),
+            'origin': json.dumps(origin),
+            'destination': json.dumps(destination),
+            'tripDirection': tripDirection,
+            'travels': json.dumps(travels),
+            'payWithPoints': 'false',
+            'currency': 'EUR',
+            'tripIntention': 'forLeisure',
+            'passengerHSelected': 'false'
+        }
 
-        time.sleep(2)
-        currentAdults = 1
-        while currentAdults < query.adults:
-            logging.info("Add passenger")
-            browser.find_element(By.XPATH, config.get("adults")).click()
-            currentAdults += 1
-            if currentAdults == query.adults:
-                # close passengers modal
-                browser.find_element(By.XPATH, "/html/body/app-root/main/b2c-view-home/div[2]/section/div/div[1]/b2c-main-search/ilsa-main-search/div/div[2]/ilsa-passengers-picker/div/div[1]").click()
+        query_string = urllib.parse.urlencode(params_dict)
+        url = f"https://iryo.eu/es/booking/travels?{query_string}"
 
-        # submit form
-        browser.find_element(By.XPATH, config.get("submit")).click()
+        webbrowser.open(url)
 
-        return ""
-
-    def findStation(self, city, stations, browser):
+    def findStation(self, city, stations):
         for station in stations:
-            if city.lower() in station.text.lower():
-                station.click()
+            if city.lower() in station.get("city").lower():
                 return station
         logging.info(f"Station for {city} not found!")
         return ""
