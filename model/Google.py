e7b05403bb3575dfdfec8816c9dc11bdfa81b236
Michal <7rdsggtwp@mozmail.com>
2025-07-17 08:35:40 +0200

    Refactor google flights

--- model/Google.py
+++ model/Google.py
@@ -1,76 +1,27 @@
-import logging
-import time
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+import webbrowser
 from .data_classes import SearchQuery
+import urllib.parse
 
 class Google:
 
     def parse(self, query: SearchQuery):
-        url = query.params.get("url")
-        config = query.params.get("params")
+        start_date = query.start_date.strftime("%Y-%m-%d")
 
-        startTrip = query.start_date.strftime(query.params.get("dateFormat"))
-        endTrip = query.end_date.strftime(query.params.get("dateFormat"))
+        query_string = f"Flights to {query.to_city} from {query.from_city} on {start_date}"
 
-        logging.info("Open Browser " + url)
-        browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-        browser.get(url)
-        browser.implicitly_wait(10)  # seconds
-
-        # accept cookies
-        if "cookiesAccept" in config:
-            cookiesAccept = browser.find_element(By.XPATH, config.get("cookiesAccept"))
-            cookiesAccept.click()
-
-        departure = browser.find_element(By.XPATH, config.get("departure"))
-        departure.clear()
-        departure.send_keys(query.from_city)
-        confirm = browser.find_element(By.CLASS_NAME, config.get("confirmAirportClass"))
-        confirm.click()
-        time.sleep(1)
-
-        arrival = browser.find_element(By.XPATH, config.get("arrival"))
-        arrival.send_keys(query.to_city)
-        confirm = browser.find_element(By.CLASS_NAME, config.get("confirmAirportClass"))
-        confirm.click()
-        time.sleep(1)
-
-        # handle passengers number
-        currentAdults = 1
-        if query.adults > 1:
-            browser.find_element(By.XPATH, config.get("adultsInit")).click()
-            time.sleep(2)
-
-        while currentAdults < query.adults:
-            browser.find_element(By.XPATH, config.get("adults")).click()
-            currentAdults += 1
-
-        if query.adults > 1:
-            browser.find_element(By.XPATH, config.get("adultsConfirm")).click()
-
-        time.sleep(2)
-
-        if not query.round_trip:
-            logging.info("Handle one way trip")
-            browser.find_element(By.XPATH, config.get("oneWayInit")).click()
-            browser.find_element(By.XPATH, config.get("oneWay")).click()
-            dateFrom = browser.find_element(By.XPATH, config.get("dateFrom"))
-            dateFrom.send_keys(startTrip)
-            dateFrom.send_keys(Keys.ENTER)
+        if query.round_trip:
+            end_date = query.end_date.strftime("%Y-%m-%d")
+            query_string += f" through {end_date}"
         else:
-            dateFrom = browser.find_element(By.XPATH, config.get("dateFrom"))
-            dateFrom.send_keys(startTrip)
-            dateBack = browser.find_element(By.XPATH, config.get("dateBack"))
-            dateBack.send_keys(endTrip)
-            dateBack.send_keys(Keys.ENTER)
+            query_string += " one way"
 
-        # submit form
-        time.sleep(2)
-        browser.find_element(By.XPATH, config.get("submit")).click()
+        if query.adults > 1:
+            query_string += f" for {query.adults} adults"
+
+        encoded_query = urllib.parse.quote(query_string)
+
+        url = f"https://www.google.com/travel/flights?q={encoded_query}"
+
+        webbrowser.open(url)
 
         return ""
