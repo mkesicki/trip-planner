c8e2b3f35f759b0002f7c6d845a70725adc4294b
Michal <7rdsggtwp@mozmail.com>
2025-07-19 23:09:22 +0200

    Fix vueling

--- model/Vueling.py
+++ model/Vueling.py
@@ -1,5 +1,5 @@
 import logging
-
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,12 +11,8 @@ from .data_classes import SearchQuery
 class Vueling:
 
     def parse(self, query: SearchQuery):
-        # they count from 0 -> January is 0
-        startMonth = query.start_date.month - 1
-        endMonth = query.end_date.month - 1
-
-        startTrip = query.start_date.strftime(query.params.get("dateFormat")).replace("{X}", str(startMonth))
-        endTrip = query.end_date.strftime(query.params.get("dateFormat")).replace("{X}", str(endMonth))
+        startTrip = query.start_date.strftime(query.params.get("dateFormat"))
+        endTrip = query.end_date.strftime(query.params.get("dateFormat"))
 
         url = query.params.get("url")
         self.config = query.params.get("params")
@@ -24,38 +20,40 @@ class Vueling:
         logging.info("Open Browser " + url)
         browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         browser.get(url)
-        browser.implicitly_wait(10)  # seconds
+        time.sleep(10)
 
         # accept cookies
         if "cookiesAccept" in self.config:
-            browser.find_element(By.ID, self.config.get("cookiesAccept")).click()
+            button = browser.find_element(By.ID, self.config.get("cookiesAccept"))
+            browser.execute_script("arguments[0].click();", button)
 
         departure = browser.find_element(By.ID, self.config.get("departure"))
         departure.send_keys(query.from_city)
-        departure.send_keys(Keys.ENTER)
-
-        browser.implicitly_wait(2)  # seconds
+        confirm = browser.find_element(By.CLASS_NAME, "vy-list-dropdown_item_button")
+        browser.execute_script("arguments[0].click();", confirm)
+        time.sleep(2)
 
         arrival = browser.find_element(By.ID, self.config.get("arrival"))
         arrival.send_keys(query.to_city)
-        arrival.send_keys(Keys.ENTER)
+        confirm = browser.find_element(By.CLASS_NAME, "vy-list-dropdown_item_button")
+        browser.execute_script("arguments[0].click();", confirm)
+        time.sleep(2)
 
-        browser.implicitly_wait(2)  # seconds
+        startTrip_input = browser.find_element(By.ID, self.config.get("dateFrom"))
+        browser.execute_script(f"arguments[0].value = '{startTrip}';", startTrip_input)
 
-        self.findDate(browser, startTrip)
-
-        if not query.round_trip:
-            logging.info("Handle one way trip")
-            browser.find_element(By.ID, self.config.get("oneWay")).click()
-            browser.implicitly_wait(5)  # seconds
-            browser.find_element(By.ID, f"calendarDaysTable{startTrip}").click()
+        if query.round_trip:
+            endTrip_input = browser.find_element(By.ID, self.config.get("dateBack"))
+            browser.execute_script(f"arguments[0].value = '{endTrip}';", endTrip_input)
         else:
-            browser.find_element(By.ID, f"calendarDaysTable{startTrip}").click()
-            browser.implicitly_wait(5)  # seconds
-            self.findDate(browser, endTrip)
-            browser.find_element(By.ID, f"calendarDaysTable{endTrip}").click()
+            logging.info("Handle one way trip")
+            browser.find_element(By.CLASS_NAME, self.config.get("oneWay")).click()
+
 
         # handle passengers number
+        passengers_input = browser.find_element(By.ID, self.config.get("passengersSelector"))
+        browser.execute_script("arguments[0].click();", passengers_input)
+        time.sleep(2)
         currentAdults = 1
         while currentAdults < query.adults:
             browser.find_element(By.ID, self.config.get("adults")).click()
@@ -66,12 +64,4 @@ class Vueling:
 
         return ""
 
-    def findDate(self, browser, date):
-        try:
-            browser.find_element(By.ID, f"calendarDaysTable{date}")
-            logging.info("Date found !")
-            return
-        except NoSuchElementException:
-            logging.info("search month in calendar")
-            browser.find_element(By.ID, self.config.get("dateForward")).click()
-            self.findDate(browser, date)
+
