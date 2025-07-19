a4549a86aed5301b70d23d3c30816f1fd936ef43
Michal <7rdsggtwp@mozmail.com>
2025-07-20 00:28:48 +0200

    Cleanup

--- model/Vueling.py
+++ model/Vueling.py
@@ -2,7 +2,6 @@ import logging
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import *
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
